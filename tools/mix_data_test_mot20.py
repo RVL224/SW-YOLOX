5b028b5ae993d6d44dd1e068e829145d0e56bcf9
Jeasonde <104689579+jeasonde@users.noreply.github.com>
2023-07-12 09:16:55 +0800

    Update mix_data_test_mot20.py

--- tools/mix_data_test_mot20.py
+++ tools/mix_data_test_mot20.py
@@ -89,60 +89,6 @@ video_list.append({
     'file_name': 'crowdhuman_val'
 })
 
-max_img = 40000
-max_ann = 20000000
-
-ethz_json = json.load(open('datasets/ETHZ/annotations/train.json','r'))
-img_id_count = 0
-for img in ethz_json['images']:
-    img_id_count += 1
-    img['file_name'] = 'ethz_train/' + img['file_name'][5:]
-    img['frame_id'] = img_id_count
-    img['prev_image_id'] = img['id'] + max_img
-    img['next_image_id'] = img['id'] + max_img
-    img['id'] = img['id'] + max_img
-    img['video_id'] = max_video
-    img_list.append(img)
-    
-for ann in ethz_json['annotations']:
-    ann['id'] = ann['id'] + max_ann
-    ann['image_id'] = ann['image_id'] + max_img
-    ann_list.append(ann)
-
-print('ETHZ')
-
-video_list.append({
-    'id': max_video,
-    'file_name': 'ethz'
-})
-
-max_img = 50000
-max_ann = 25000000
-
-cp_json = json.load(open('datasets/Cityscapes/annotations/train.json','r'))
-img_id_count = 0
-for img in cp_json['images']:
-    img_id_count += 1
-    img['file_name'] = 'cp_train/' + img['file_name'][11:]
-    img['frame_id'] = img_id_count
-    img['prev_image_id'] = img['id'] + max_img
-    img['next_image_id'] = img['id'] + max_img
-    img['id'] = img['id'] + max_img
-    img['video_id'] = max_video
-    img_list.append(img)
-    
-for ann in cp_json['annotations']:
-    ann['id'] = ann['id'] + max_ann
-    ann['image_id'] = ann['image_id'] + max_img
-    ann_list.append(ann)
-
-print('Cityscapes')
-
-video_list.append({
-    'id': max_video,
-    'file_name': 'cityperson'
-})
-
 mix_json = dict()
 mix_json['images'] = img_list
 mix_json['annotations'] = ann_list
